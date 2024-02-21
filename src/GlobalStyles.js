fef2e245939d6679f1ec387d73805e3c294805c3
Anton240896 <antonalipov791@gmail.com>
2024-02-21 10:20:17 +0200

    preparation project

---
+++ src/GlobalStyles.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+import 'modern-normalize';
+
+export const GlobalStyles = createGlobalStyle`
+
+
+
+body {
+  margin: 0;
+}
+
+
+ul,
+ol {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+a {
+  text-decoration: none;
+}
+
+.button {
+  cursor: pointer;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+
+code {
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+    monospace;
+}
