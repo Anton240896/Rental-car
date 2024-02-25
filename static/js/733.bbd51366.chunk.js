2947e332db9f5b25bd5e68ad788318c40e5eafcc
Anton240896 <antonalipov240811111111@gmail.com>
2024-02-25 10:03:41 +0000

    Deploying to gh-pages from @ Anton240896/Rental-car@95f1f48d42ebc1caefff04a9e56ad4a8c09008a4 🚀

---
+++ static/js/733.bbd51366.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkRental_car=self.webpackChunkRental_car||[]).push([[733],{3733:function(n,e,r){r.r(e),r.d(e,{default:function(){return i}});var t=r(7689),a=r(1087),c=r(184);function i(){var n,e,r=(0,t.TH)();return(0,c.jsxs)("div",{children:[(0,c.jsx)("p",{children:"Favorites"}),(0,c.jsx)(a.rU,{to:null!==(n=null===(e=r.state)||void 0===e?void 0:e.from)&&void 0!==n?n:"/",children:"Home back "})]})}}}]);
+//# sourceMappingURL=733.bbd51366.chunk.js.map
